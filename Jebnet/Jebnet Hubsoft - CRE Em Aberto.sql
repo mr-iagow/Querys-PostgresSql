018039d5b420edd8bfcd176bda10c3ffc39eb646
Iago Oliveira <135724820+mr-iagow@users.noreply.github.com>
2025-08-16 10:40:49 -0300

    Inclusão de relatórios e separação de consultas por empresas

---
+++ Jebnet/Jebnet Hubsoft - CRE Em Aberto.sql
@@ -0,0 +1,29 @@
+SELECT 
+ct.nome_razaosocial,
+ss.descricao,
+fc.descricao AS tipo_cobranca,
+fat.numero_controle,
+cs.tipo_cobranca,
+serv.descricao,
+fat.nosso_numero_dv,
+fat.valor,
+fat.valor_pago,
+fat.data_vencimento,
+fat.linha_digitavel,
+fat.codigo_barras
+
+FROM fatura AS fat
+JOIN forma_cobranca AS fc ON fc.id_forma_cobranca = fat.id_forma_cobranca
+JOIN cliente_servico AS cs ON cs.id_cliente_servico = fat.id_cliente_servico
+JOIN servico AS serv ON serv.id_servico = cs.id_servico
+LEFT JOIN cliente AS ct ON ct.id_cliente = cs.id_cliente
+JOIN servico_status AS ss ON ss.id_servico_status = cs.id_servico_status
+
+WHERE 
+
+
+fat.data_vencimento <= '2025-09-30'
+AND fat.valor_pago IS NULL 
+AND fat.ativo = TRUE 
+
+ORDER BY ct.id_cliente
---
+++ Jebnet/Jebnet Migracao - CRE Em Aberto.sql
@@ -0,0 +1,37 @@
+SELECT --DISTINCT ON (frt.id)
+
+ba."description" AS banco,
+p."name" AS cliente,
+tt."name" AS tipo_cliente,
+frt.competence AS competencia,
+c.contract_number AS contrato,
+frtt.receipt_date AS data_recebimento,
+frt.expiration_date AS data_vencimento,
+frt.title_amount AS valor,
+frtt.fine_amount AS multa,
+frtt.increase_amount AS juros,
+frtt.discount_value AS desconto,
+frt.issue_date AS emissao,
+fo_titulo.title AS operacao_financeira,
+fn.title AS natureza_financeira,
+frt.origin AS origem,
+frtt.amount AS recebimento,
+fct.title AS tipo_cobranca,
+frt.title AS titulo,
+frt.title_amount AS valor_titulo
+
+
+FROM financial_receivable_titles AS frt
+LEFT JOIN financial_receipt_titles AS frtt ON frtt.financial_receivable_title_id = frt.id
+JOIN bank_accounts AS ba ON ba.id = frt.bank_account_id
+JOIN people AS p ON p.id = frt.client_id
+JOIN tx_types AS tt ON tt.id = p.type_tx_id
+LEFT JOIN contracts AS c ON c.id = frt.contract_id
+JOIN financial_operations AS fo_titulo ON fo_titulo.id = frt.financial_operation_id
+JOIN financers_natures AS fn ON frt.financer_nature_id = fn.id
+JOIN financial_collection_types AS fct ON fct.id = frt.financial_collection_type_id
+
+WHERE 
+
+frt.financial_collection_type_id IN (137,136,135,134,133,132,131)
+AND frt.title LIKE '%FAT%'
