4460789b5640a4b5f389553f7f23ea544761d4f9
Iago Oliveira <135724820+mr-iagow@users.noreply.github.com>
2025-10-09 11:23:28 -0300

    Update Faturamento - Contratos Por Data De Faturar De Inicial.sql

--- Faturamento - Contratos Por Data De Faturar De Inicial.sql
+++ Faturamento - Contratos Por Data De Faturar De Inicial.sql
@@ -4,9 +4,11 @@ ct.contract_number AS numero_contrato,
 (SELECT p.name FROM people AS p WHERE p.id = ct.client_id) AS cliente,
 ct.billing_beginning_date AS data_inicio_faturamento,
 (SELECT ctt.title FROM contract_types AS ctt WHERE ctt.id = ct.contract_type_id) AS tipo_contrato,
+cp."description" AS empresa,
 ct.approval_date AS data_aprovacao,
 ct.v_status AS status_contrato
 
 FROM contracts AS ct
+JOIN companies_places AS cp ON cp.id = ct.company_place_id
 
-WHERE DATE (ct.billing_beginning_date) >= '2025-10-01'
+WHERE DATE (ct.billing_beginning_date) >= '2025-12-01'
