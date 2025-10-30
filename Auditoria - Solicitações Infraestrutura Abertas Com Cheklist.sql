5c84692a4bd83eeea7149829ef1af89604162e32
Iago Oliveira <135724820+mr-iagow@users.noreply.github.com>
2025-10-30 11:15:12 -0300

    Create Auditoria - Solicitações Infraestrutura Abertas Com Cheklist.sql

---
+++ Auditoria - Solicitações Infraestrutura Abertas Com Cheklist.sql
@@ -0,0 +1,30 @@
+SELECT DISTINCT ON (ai.protocol)
+ai.protocol AS protocolo,
+ins.title as status_solicitacao,
+it.title AS tipo_solicitacao,
+v."name" AS aberto_por,
+DATE (a.created) AS data_abertura,
+DATE (a.conclusion_date) AS data_encerramento,
+(SELECT p.name FROM people AS p WHERE p.id = a.responsible_id ) AS tecnico_responsavel,
+a.description AS relato_abertura,
+--LAST_VALUE(r.description) OVER (PARTITION BY a.id ORDER BY r.created ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS relato_encerramento,
+   		   (ai.final_checklist::jsonb)->'0_'->>'label' AS chek_1,
+				(ai.final_checklist::jsonb)->'0_'->>'value' AS resposta_1,
+            (ai.final_checklist::jsonb)->'1_'->>'label' AS chek_2,
+			   CASE 
+            WHEN (ai.final_checklist::jsonb)->'1_'->>'value' = '1' THEN 'sim'
+            WHEN (ai.final_checklist::jsonb)->'1_'->>'value' = '0' THEN 'Nao'
+       		END AS resposta_2,
+            (ai.beginning_checklist::jsonb)->'0_'->>'label' AS chek_3,
+				(ai.beginning_checklist::jsonb)->'0_'->>'value' AS resposta_3
+   
+FROM assignments AS a
+JOIN assignment_incidents AS ai ON ai.assignment_id = a.id
+LEFT JOIN reports AS r ON r.assignment_id = a.id
+JOIN incident_status as ins on ins.id = ai.incident_status_id
+JOIN incident_types AS it ON it.id = ai.incident_type_id
+JOIN v_users AS v ON v.id = a.created_by
+
+WHERE 
+    DATE (a.created) BETWEEN '2025-07-01' AND '2025-10-30'
+    AND ai.team_id = 4 
