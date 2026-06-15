3dd14493a3be2249dfaa46ec534aa0eb74924b9e
Iago Oliveira <135724820+mr-iagow@users.noreply.github.com>
2026-06-15 17:25:27 -0300

    Update Financeiro - Faturamento V2 atualizado.sql

--- Financeiro - Faturamento V2 atualizado.sql
+++ Financeiro - Faturamento V2 atualizado.sql
@@ -58,5 +58,6 @@ AND notas.financial_operation_id IN (1,15,25,26,34,46,63,65,104)
 AND notas.status NOT IN (4,5)
 AND notas.id NOT IN (975130 /*, 967966*/)
 AND notas.document_number IS NOT NULL 
+AND notas.status != 3 -- Remoção de notas com status aguardando emissão
 --AND notas.document_number = 7367
 GROUP BY notas.id, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39
