c641970cf7324fa521869f229654bf46d08494d0
Iago Oliveira <135724820+mr-iagow@users.noreply.github.com>
2026-06-29 13:00:40 -0300

    Update Auditoria - Notas Fiscais De Entrada De Pedido De Compra.sql

--- Auditoria - Notas Fiscais De Entrada De Pedido De Compra.sql
+++ Auditoria - Notas Fiscais De Entrada De Pedido De Compra.sql
@@ -3,6 +3,7 @@ inv.document_number AS numero_documento,
 inv.entry_date AS data_entrada_nota,
 p.name AS fornecedor,
 fo.title AS operacao,
+fn.title AS natureza_financeira,
 cp.description AS empresa_compra_item,
 inv.product_acquisition_request_id AS numero_pedido_compra,
 invt."description" AS item_nota_compra,
@@ -21,8 +22,9 @@ JOIN people AS p ON p.id = inv.supplier_id
 JOIN financial_operations AS fo ON fo.id = inv.financial_operation_id
 LEFT JOIN patrimonies AS pat ON pat.entry_invoice_note_id = inv.id
 LEFT JOIN v_users AS v ON v.id = pat.created_by
+JOIN financers_natures AS fn ON fn.id = inv.financer_nature_id
 
 
 WHERE 
-DATE (inv.entry_date) BETWEEN '$data01' AND '$data02'
+DATE (inv.entry_date) BETWEEN '2026-06-20' AND '2026-06-25'
 AND inv.product_acquisition_request_id IS NOT NULL 
